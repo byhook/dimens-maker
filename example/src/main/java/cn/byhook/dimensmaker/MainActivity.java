c6db5e2a853b93c873258bfa88059ee4f2286875
byhook <byhook@163.com>
2024-01-08 15:16:01 +0800

    升级gradle版本&修正失效的图片引用

--- example/src/main/java/cn/byhook/dimensmaker/MainActivity.java
+++ example/src/main/java/cn/byhook/dimensmaker/MainActivity.java
@@ -1,9 +1,9 @@
 package cn.byhook.dimensmaker;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.DisplayMetrics;
 import android.widget.TextView;
+import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
 
